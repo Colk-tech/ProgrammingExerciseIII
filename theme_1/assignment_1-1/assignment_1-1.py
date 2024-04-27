8ced6ad1bed83c48d80b879437752d52d5e3f317
Colk-tech <master@colk.dev>
2024-04-28 00:15:10 +0900

    add: add header and `main` in `assignment_1-1.py`

---
+++ theme_1/assignment_1-1/assignment_1-1.py
@@ -0,0 +1,8 @@
+# Assignment '1-1' of Programming Exercise III at Ibaraki University, 2024
+# Created on Apr 24, 2024 by 伊藤 愛基 / ITO Manaki (22T4085Y)
+#
+# This program is published on the internet at the following URL:
+# https://github.com/Colk-tech/ProgrammingExerciseIII
+
+if __name__ == "__main__":
+    pass
