525be565d8c0423fc2d4b80a7b9c8a477498013c
Colk-tech <master@colk.dev>
2024-05-13 23:01:33 +0900

    add: add warning text in the top

--- theme_1/assignment_1-1/22T4085Y.py
+++ theme_1/assignment_1-1/22T4085Y.py
@@ -3,6 +3,14 @@
 #
 # This program is published on the internet at the following URL:
 # https://github.com/Colk-tech/ProgrammingExerciseIII
+#
+# NOTE: If you notice that this program or a program very similar to this one is submitted as an assignment,
+# it is likely that THE PERSON IN QUESTION has infringed the academic integrity policy.
+# THIS PROGRAM IS THE ORIGINAL. DO NOT HAND IN THIS PROGRAM DESPITE IT IS PUBLIC.
+# 注意: もしもこのプログラム、あるいは非常に類似のプログラムが課題として提出されているのを発見した場合、
+# その者が学術倫理方針に違反している可能性があります。
+# このプログラムがオリジナルです。このプログラムは公開されていますが、提出用に使用しないでください。
+
 
 if __name__ == "__main__":
     pass
